e1ba76dee3d58d25885e821642484561c999d416
lbarrow91 <laurabarrow1991@gmail.com>
2023-02-21 18:28:22 +0000

    adding function to write README file

--- assets/index.js
+++ assets/index.js
@@ -56,6 +56,13 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Success!');
+        }
+    });
 }
 
 // function to initialize program
